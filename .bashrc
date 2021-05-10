5f3b3e24242f491cd095e8e897ce9b81589f4039
hujuran <hujuran@outlook.com>
2021-05-10 17:39:50 +0800

    name

--- .bashrc
+++
@@ -1,4 +0,0 @@
-alias du1="du -h --max-depth=1"
-alias up1="docker-compose up"
-alias up2="docker-compose up -d"
-alias down1="docker-compose down"
---
+++ myalias.sh
@@ -0,0 +1,8 @@
+alias du1="du -h --max-depth=1"
+alias up1="docker-compose up"
+alias up2="docker-compose up -d"
+alias down1="docker-compose down"
+alias c='clear'
+alias ..='cd ..'
+alias ports='netstat -tulanp'
+alias lh='ls -lh'
