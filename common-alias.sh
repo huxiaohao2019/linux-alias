a641dc9afaf554932d1eb7ab79ea06be6141737e
hujuran <hujuran@outlook.com>
2021-05-10 17:50:48 +0800

    1

---
+++ common-alias.sh
@@ -0,0 +1,5 @@
+alias c='clear'
+alias lh='ls -lh'
+alias ..='cd ..'
+alias du1="du -h --max-depth=1"
+alias ports='netstat -tulanp'
--- myalias.sh
+++
@@ -1,8 +0,0 @@
-alias du1="du -h --max-depth=1"
-alias up1="docker-compose up"
-alias up2="docker-compose up -d"
-alias down1="docker-compose down"
-alias c='clear'
-alias ..='cd ..'
-alias ports='netstat -tulanp'
-alias lh='ls -lh'
